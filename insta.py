3d5425f2e7fce43af3432a8a0178bd9e16ead828
Matheus Telles <mwtelles2012@hotmail.com>
2020-03-20 15:07:10 -0300

    your username and password

--- insta.py
+++ insta.py
@@ -1,13 +1,13 @@
 from instapy import InstaPy
 from instapy import smart_run
 
-session = InstaPy(username='dixxtelleszin', password='24719860')
+session = InstaPy(username='yourlogin', password='yourpassword')
 
 with smart_run(session):
 	session.set_do_follow(enabled = True, percentage = 20)
 	session.set_do_like(enabled = True, percentage= 100)
 
-	session.like_by_tags(['skywalker'], amount=200)
+	session.like_by_tags(['gameficada'], amount=200)
 
 	comentarios = ['Awesome !', 'May the force be with u']
 	session.set_do_comment(enabled=True, percentage=95)
